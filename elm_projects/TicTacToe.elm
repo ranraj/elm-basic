9c4540944ca3a75643b740570247ffd2b07007e8
ranjithraj.d <ranjithraj.d@gmail.com>
2016-06-01 12:44:16 +0530

    TicTacToe working version with result

--- elm_projects/TicTacToe.elm
+++ elm_projects/TicTacToe.elm
@@ -7,7 +7,7 @@ import Html.Events exposing (onClick)
 main = App.program{init=init,view = view ,update = update,subscriptions= subscriptions}
 
 type Player = PlayerO | PlayerX | Empty
-type Status = LastMove String | Default
+type Status = LastMove String | Default | NotValidMove String
 type alias Cell = {
     position : (Int,Int),    
     player : Player    
@@ -20,7 +20,7 @@ type alias Model = {
 }
 model = Model drawBoardList Default PlayerO
 
-type Msg = Played (Int,Int)
+type Msg = Played (Int,Int) | FindResult
 
 update : Msg -> Model -> (Model,Cmd Msg)
 update msg model =  case msg of
@@ -36,17 +36,28 @@ update msg model =  case msg of
        ,nextPlayer = if model.nextPlayer == PlayerO then PlayerX else PlayerO
     },Cmd.none)
 
+  FindResult -> ({
+    model |         
+       status = case (findWinnerSequence PlayerO model.board winSequence) of
+        True -> LastMove "Player O Wins the Game"
+        False -> case (findWinnerSequence PlayerX model.board winSequence) of
+          True -> LastMove "Player X Wins the Game"
+          False -> LastMove "Tie"            
+    },Cmd.none)
+
 notification status = case status of
     Default -> "Welcome.."
     LastMove msg -> "LastMove - " ++ msg
+    NotValidMove msg -> "Exception - " ++ msg
 
 view model = div [] [
-  div[] [Html.text (notification model.status)] 
+  div[] [Html.text (notification model.status)]
   ,Svg.svg [ version "1.1", x "0", y "0", viewBox "0 0" ,width "300",height "300"] (drawBoard model.board)  
+  ,div [] [ button [onClick FindResult] [Html.text "Result"]]
   ]
 
 drawBoardList = [
-  Cell (0,0) Empty
+   Cell (0,0) Empty
   ,Cell (0,1) Empty
   ,Cell (0,2) Empty
   ,Cell (1,0) Empty
@@ -56,6 +67,34 @@ drawBoardList = [
   ,Cell (2,1) Empty
   ,Cell (2,2) Empty 
   ]
+
+winSequence = [
+ [(0,0),(0,1),(0,2)]
+ ,[(0,0),(1,0),(2,0)]
+ ,[(0,0),(1,1),(2,2)]
+ ,[(0,1),(1,1),(2,1)]
+ ,[(1,0),(1,1),(1,2)]
+ ,[(2,0),(2,1),(2,2)]
+ ,[(0,2),(1,2),(2,2)]
+ ,[(0,2),(1,1),(2,0)]
+ ]
+
+whoWin player board l = containsAll (fetchCellsByPlayer player board) l 
+fetchCellsByPlayer player board = List.filter (\x -> x.player == player) board |> List.map (\x -> x.position)
+
+findWinnerSequence player board l = case l of
+  [] -> False
+  (x::xs) -> case (whoWin player board x) of
+    True -> True
+    False -> findWinnerSequence player board xs
+
+containsAll mainl subl = 
+  case subl of
+    [] -> True
+    (x::xs) -> case (List.member x mainl) of
+       True -> containsAll mainl xs
+       False -> False
+
 
 drawBoard board = List.map  drawBoardMap board
 drawBoardMap cell = 
